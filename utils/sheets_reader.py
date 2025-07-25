f302ea5c39e77d7ee9f462d53eeb67b709b6df0f
rfullivarri <113726007+rfullivarri@users.noreply.github.com>
2025-07-26 00:56:06 +0200

    Update sheets_reader.py

--- utils/sheets_reader.py
+++ utils/sheets_reader.py
@@ -1,7 +1,7 @@
 import gspread
 import pandas as pd
 import re
-from google.oauth2.service_account import Credentials
+
 from oauth2client.service_account import ServiceAccountCredentials
 import streamlit as st
 
@@ -72,7 +72,9 @@ def get_gamification_data(email):
 
 
 def update_avatar_url(email, url):
-    creds = Credentials.from_service_account_info(st.secrets["google_service_account"])
+    # Autenticación con Google
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["google_service_account"], scope)
     client = gspread.authorize(creds)
 
     # Abre el archivo central
